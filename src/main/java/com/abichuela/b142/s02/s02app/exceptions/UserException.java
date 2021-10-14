07404d3f22f10d7a21fe88937be2f0ab8b73da57
BJ Abichuela <bjabichuela@gmail.com>
2021-10-14 21:34:52 +0800

    #14 - Create the exceptions directory and files

---
+++ src/main/java/com/abichuela/b142/s02/s02app/exceptions/UserException.java
@@ -0,0 +1,9 @@
+package com.abichuela.b142.s02.s02app.exceptions;
+
+public class UserException extends Exception{
+
+    // Parameterized constructor
+    public UserException(String message) {
+        super(message);
+    }
+}
