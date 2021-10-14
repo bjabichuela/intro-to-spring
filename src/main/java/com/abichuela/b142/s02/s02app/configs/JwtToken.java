41c04c90da1ca4a4ad9fda7fc0ebac01452f2751
BJ Abichuela <bjabichuela@gmail.com>
2021-10-14 21:33:21 +0800

    # - Create the configs directory and files

---
+++ src/main/java/com/abichuela/b142/s02/s02app/configs/JwtToken.java
@@ -0,0 +1,96 @@
+package com.abichuela.b142.s02.s02app.configs;
+
+import com.abichuela.b142.s02.s02app.models.User;
+import com.abichuela.b142.s02.s02app.repositories.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+//To generate token
+@Component
+public class JwtToken implements Serializable {
+
+
+    @Value("${jwt.secret}")//taken from application.properties
+    private String secret;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final long serialVersionUID = -2550185165626007488L;
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60; //5hrs
+
+
+
+    public String getUsernameFromToken(String token) {
+        String claim = getClaimFromToken(token, Claims::getSubject);
+        return claim;
+
+    }
+
+
+    public Date getExpirationDateFromToken(String token) {
+
+        return getClaimFromToken(token, Claims::getExpiration);
+
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+
+        final Claims claims = getAllClaimsFromToken(token);
+
+        return claimsResolver.apply(claims);
+
+    }
+
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+
+    private Boolean isTokenExpired(String token) {
+
+        final Date expiration = getExpirationDateFromToken(token);
+
+        return expiration.before(new Date());
+
+    }
+
+
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>();
+        User user = userRepository.findByUserName(userDetails.getUsername());
+        claims.put("user", user.getId());
+        return doGenerateToken(claims, userDetails.getUsername());
+
+    }
+
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+
+    }
+
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+}
---
+++ src/main/java/com/abichuela/b142/s02/s02app/configs/WebSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.abichuela.b142.s02.s02app.configs;
+
+import com.abichuela.b142.s02.s02app.services.UserDetailsServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticate jwtAuthenticate;
+
+    @Autowired
+    private UserDetailsServiceImplementation jwtUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+
+    @Bean
+    public JwtAuthenticate jwtAuthenticationEntryPointBean() throws Exception{
+        return new JwtAuthenticate();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+
+        httpSecurity.cors().and().csrf().disable()
+
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers("/users/register").permitAll()
+                .antMatchers(HttpMethod.GET, "/posts").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated().and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticate).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+}
+
