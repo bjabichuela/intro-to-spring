b150a086b824f3fbeb40c6e14f3f6c86cacf3ebb
BJ Abichuela <bjabichuela@gmail.com>
2021-10-14 21:27:45 +0800

    # Add findByUserName method in UserRepository

--- src/main/java/com/abichuela/b142/s02/s02app/repositories/UserRepository.java
+++ src/main/java/com/abichuela/b142/s02/s02app/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.abichuela.b142.s02.s02app.repositories;
 
 import com.abichuela.b142.s02.s02app.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Object> {
+    User findByUserName(String username);
 }
